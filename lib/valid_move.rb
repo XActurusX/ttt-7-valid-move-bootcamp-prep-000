93bc1da1ae3f7eb51fc1e81e43d99112f0820f94
John Jordan Shelley <jshelley553@gmail.com>
2019-06-08 04:47:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@ def valid_move?(board, index)
   if position_taken?(board, index)
     return false
   end
-  unless board[index].between?(0, 8)
+  unless board[index].between?(1, 9)
     return false
   end
   return true
